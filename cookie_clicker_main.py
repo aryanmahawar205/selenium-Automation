e4a64778319a96326cedadb05c2c82a108c46863
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-06-11 18:18:16 +0530

    full code

--- cookie_clicker_main.py
+++ cookie_clicker_main.py
@@ -10,6 +10,38 @@ service = Service(executable_path = "chromedriver.exe")
 driver = webdriver.Chrome(service = service)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-time.sleep(10)
+cookie_ID = "bigCookie"
+cookies_count_ID = "cookies"
+product_price_prefix = "productPrice"
+product_prefix = "product"
 
-driver.quit()
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]")))
+
+language = driver.find_element(By.XPATH, "//*[contains(text(), 'English')]")
+language.click()
+
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, cookie_ID)))
+
+cookie = driver.find_element(By.ID, cookie_ID)
+
+#time.sleep(10)
+
+#driver.quit()
+
+while True:
+    cookie.click()
+    cookies_count = driver.find_element(By.ID, cookies_count_ID).text.split(" ")[0]
+    cookies_count = int(cookies_count.replace(",", ""))
+    
+    for i in range(4):
+        product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+        
+        if not product_price.isdigit():
+            continue
+        
+        product_price = int(product_price)
+        
+        if (cookies_count >= product_price):
+            product = driver.find_element(By.ID, product_prefix + str(i))
+            product.click()
+            break
